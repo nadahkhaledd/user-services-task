c2d42d917e1a5a190b23b4fb80aa50b13aad912f
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-16 14:43:22 +0200

    added user & service repos

---
+++ src/main/java/com/example/usm/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.usm.repository;
+
+import com.example.usm.entity.Service;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, Long> {
+
+
+}
