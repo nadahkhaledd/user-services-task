ac39c730a8844b071829e5093e71fa5b5c6c7032
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-16 10:49:42 +0200

    initialized dependencies

---
+++ src/main/java/com/example/usm/UserservicemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.usm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserservicemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserservicemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usm/UserservicemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserservicemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
