86d2971155d69f4c93bbba7409464950ec776be8
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-17 17:10:22 +0200

    implemented ServicesController

---
+++ src/main/java/com/example/usm/controller/ServicesController.java
@@ -0,0 +1,62 @@
+package com.example.usm.controller;
+
+import com.example.usm.dto.ServiceDTO;
+import com.example.usm.dto.UserDTO;
+import com.example.usm.entity.Service;
+import com.example.usm.entity.User;
+import com.example.usm.exception.service.ServiceNotFoundException;
+import com.example.usm.exception.user.UserNotFoundException;
+import com.example.usm.service.services.IServicesService;
+import com.example.usm.service.user.IUserService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/services")
+public class ServicesController {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private final IServicesService servicesService;
+
+    @Autowired
+    public ServicesController(IUserService userService, IServicesService servicesService){
+        this.servicesService = servicesService;
+    }
+
+    @GetMapping
+    public Object find(){
+        return servicesService.getAll().stream()
+                .map(service -> modelMapper.map(service, ServiceDTO.class)).toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ServiceDTO addService(@Valid @RequestBody ServiceDTO serviceDTO){
+        Service service = servicesService.add(modelMapper.map(serviceDTO, Service.class));
+        return modelMapper.map(service, ServiceDTO.class);
+    }
+
+    @GetMapping("/{uid}")
+    @ResponseStatus(HttpStatus.OK)
+    public ServiceDTO findByUID(@PathVariable(name = "uid") long uid){
+        return modelMapper.map(servicesService.findByUID(uid), ServiceDTO.class);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ServiceNotFoundException.class)
+    public String return400s(ServiceNotFoundException ex) {
+        return ex.getMessage();
+
+    }
+
+
+
+
+
+
+}
