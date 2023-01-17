508c82ae50e4a83941431a66066b8f72a09ce20f
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-17 10:40:44 +0200

    added User service

---
+++ src/main/java/com/example/usm/service/user/IUserService.java
@@ -0,0 +1,20 @@
+package com.example.usm.service.user;
+
+import com.example.usm.entity.User;
+import com.example.usm.enums.UserType;
+
+import java.util.List;
+
+ interface IUserService {
+
+     User add(User user);
+
+     List<User> getAll();
+
+     User findBySN(String serialNumber);
+
+     User findByPhone(String phoneNumber);
+
+     List<User> findByType(UserType type);
+
+}
---
+++ src/main/java/com/example/usm/service/user/UserService.java
@@ -0,0 +1,47 @@
+package com.example.usm.service.user;
+
+import com.example.usm.entity.User;
+import com.example.usm.enums.UserType;
+import com.example.usm.exception.user.FieldName;
+import com.example.usm.exception.user.UserNotFoundException;
+import com.example.usm.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IUserService{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User add(User user) {
+        return userRepository.findById(user.getSerialNumber()).orElse(userRepository.save(user));
+    }
+
+    @Override
+    public List<User> getAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public User findBySN(String serialNumber) {
+        return userRepository.findById(serialNumber).orElseThrow(() -> new UserNotFoundException(FieldName.SerialNumber));
+    }
+
+    @Override
+    public User findByPhone(String phoneNumber) {
+        return userRepository.findByPhoneNumber(phoneNumber).orElseThrow(() -> new UserNotFoundException(FieldName.SerialNumber));
+    }
+
+    @Override
+    public List<User> findByType(UserType type) {
+        return userRepository.findByType(type);
+    }
+}
