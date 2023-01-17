bfaee57b2dc423b95d85fdf0170c8bb0c8e10e3c
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-17 10:37:16 +0200

    added find by field to user & service repos

--- src/main/java/com/example/usm/repository/ServiceRepository.java
+++ src/main/java/com/example/usm/repository/ServiceRepository.java
@@ -1,11 +1,20 @@
 package com.example.usm.repository;
 
 import com.example.usm.entity.Service;
+import com.example.usm.enums.ServiceStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServiceRepository extends CrudRepository<Service, Long> {
+
+    public List<Service> findByVendor(String vendor);
+
+    public List<Service> findByUserSerialNumber(String serialNumber);
+
+    public List<Service> findByStatus(ServiceStatus status);
 
 
 }
