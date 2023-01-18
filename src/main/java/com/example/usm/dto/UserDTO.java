077a332ef5438be93eeae06ad7989376aecef4da
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-18 10:28:44 +0200

    added services changes

--- src/main/java/com/example/usm/dto/UserDTO.java
+++ src/main/java/com/example/usm/dto/UserDTO.java
@@ -2,16 +2,13 @@ package com.example.usm.dto;
 
 import com.example.usm.entity.Service;
 import com.example.usm.enums.UserType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @AllArgsConstructor
-@NoArgsConstructor
-@Getter
+@Setter @Getter
 @Builder
 public class UserDTO {
 
@@ -20,4 +17,8 @@ public class UserDTO {
     private String phoneNumber;
     private UserType type;
     private Set<Service> services;
+
+    public UserDTO (){
+        this.services = new HashSet<>();
+    }
 }
