983481dd2d673bc7b0c817810f548754fca08798
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-20 19:42:05 +0200

    added couchbase configurations

--- src/main/java/com/example/usm/UserservicemanagementApplication.java
+++ src/main/java/com/example/usm/UserservicemanagementApplication.java
@@ -1,17 +1,21 @@
 package com.example.usm;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.usm"})
+@EnableCouchbaseRepositories(basePackages = {"com.example.usm"})
 public class UserservicemanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserservicemanagementApplication.class, args);
 	}
+
 
 }
