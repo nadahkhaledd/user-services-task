82702445ede97606208d190fd17fee96f4756a4a
Nadah Khaled <nadahkhaledd@gmail.com>
2023-01-20 19:34:45 +0200

    migrated to couchbase and updated queries

--- src/main/java/com/example/usm/UserservicemanagementApplication.java
+++ src/main/java/com/example/usm/UserservicemanagementApplication.java
@@ -1,17 +1,29 @@
 package com.example.usm;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.usm"})
+@EnableCouchbaseRepositories(basePackages = {"com.example.usm"})
 public class UserservicemanagementApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserservicemanagementApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/example/usm/repository/UserRepository.java
+++ src/main/java/com/example/usm/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.usm.repository;
 
+import com.example.usm.entity.Service;
 import com.example.usm.entity.User;
 import com.example.usm.enums.UserType;
 import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
+import org.springframework.data.couchbase.core.query.ViewIndexed;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.couchbase.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,9 +16,26 @@ import java.util.Optional;
 
 @Repository
 @N1qlPrimaryIndexed
+@ViewIndexed(designDoc = "user")
 public interface UserRepository extends CouchbaseRepository<User, String> {
 
     Optional<User> findByPhoneNumber(String phoneNumber);
 
     List<User> findByType(UserType type);
+
+    @Query("""
+            SELECT ARRAY_COUNT(services) AS total_services
+            FROM #{#n1ql.bucket}
+            WHERE meta().id = $1
+            """)
+    int findNumberOfUserServices(String serialNumber);
+
+    @Transactional
+    @Query("""
+            UPDATE #{#n1ql.bucket}
+            SET services = ARRAY_APPEND(services, $1)
+            WHERE meta().id= $2""")
+    void addService(Service service, String serialNumber);
+
+
 }
